965a6fd94ebcef87befef80b4e581d424b1897c0
James Yost <jeyost@mars.cs.colostate.edu>
2023-11-15 12:10:14 -0700

    stuff?

--- test_start.py
+++ test_start.py
@@ -1,21 +1,22 @@
-from bertserini.bertserini.reader.base import Question, Context
-from bertserini.bertserini.reader.bert_reader import BERT
-from bertserini.bertserini.utils.utils_new import get_best_answer
-
-model_name = "rsvp-ai/bertserini-bert-base-squad"
-tokenizer_name = "rsvp-ai/bertserini-bert-base-squad"
-bert_reader = BERT(model_name, tokenizer_name)
+from bertserini.reader.base import Question, Context
+from bertserini.reader.bert_reader import BERT
+from bertserini.utils.utils_new import get_best_answer
+from bertserini.experiments.args import *
+args.model_name_or_path = "rsvp-ai/bertserini-bert-base-squad"
+args.tokenizer_name = "rsvp-ai/bertserini-bert-base-squad"
+bert_reader = BERT(args)
 
 # Here is our question:
-question = Question("Why did Mark Twain call the 19th century the glied age?")
+question = Question("Who was the president in 2014")
 
 # Option 1: fetch some contexts from Wikipedia with Pyserini
-from bertserini.bertserini.retriever.pyserini_retriever import retriever, build_searcher
-searcher = build_searcher("indexes/lucene-index.enwiki-20180701-paragraphs")
+from bertserini.retriever.pyserini_retriever import retriever, build_searcher
+args.index_path = "./indexes/lucene-index.enwiki-20180701-paragraphs"
+searcher = build_searcher(args)
 contexts = retriever(question, searcher, 10)
 
 # Option 2: hard-coded contexts
-contexts = [Context('The "Gilded Age" was a term that Mark Twain used to describe the period of the late 19th century when there had been a dramatic expansion of American wealth and prosperity.')]
+#contexts = [Context('The "Gilded Age" was a term that Mark Twain used to describe the period of the late 19th century when there had been a dramatic expansion of American wealth and prosperity.')]
 
 # Either option, we can ten get the answer candidates by reader
 # and then select out the best answer based on the linear 
